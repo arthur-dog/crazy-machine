dfa757c7d3b60e69ebaa40f98bb8c86f87122c1a
Ada Stevenson <adanskana@gmail.com>
2025-05-04 18:33:16 +0800

    utils: add additional helper functions

--- src/vhdl/utils.vhd
+++ src/vhdl/utils.vhd
@@ -4,27 +4,55 @@ use IEEE.numeric_std.all;
 
 package utils is
 
-function map_range (
-    input : unsigned;
-    input_start : unsigned;
-    input_end : unsigned;
-    output_start : unsigned;
-    output_end : unsigned
-) return unsigned;
+    subtype percent is natural range 0 to 100;
+    subtype servo_range_degrees is natural range 0 to 180;
+    subtype ubyte is unsigned(7 downto 0);
 
-function percent_to_byte (
-    percent : unsigned
-) return unsigned;
+    function map_range (
+        input_value  : unsigned;
+        input_start  : unsigned;
+        input_end    : unsigned;
+        output_start : unsigned;
+        output_end   : unsigned
+    ) return unsigned;
+
+    function percent_to_ubyte (
+        percent_val : percent
+    ) return ubyte;
+
+    function servo_range_degrees_to_ubyte (
+        degrees : servo_range_degrees
+    ) return ubyte;
 
 end utils;
 
 package body utils is
 
     function map_range (
-    input : unsigned;
-    input_start : unsigned;
-    input_end : unsigned;
-    output_start : unsigned;
-    output_end : unsigned
-) return
+        input_value  : unsigned;
+        input_start  : unsigned;
+        input_end    : unsigned;
+        output_start : unsigned;
+        output_end   : unsigned
+    ) return unsigned is
+    begin
+        return output_start + ((output_end - output_start) / (input_end - input_start)) * (input_value - input_start);
+    end function;
+
+    function percent_to_ubyte (
+        percent_val : percent
+    ) return ubyte is
+    begin
+        return resize(map_range(to_unsigned(percent_val, 8), to_unsigned(percent'low, 1), to_unsigned(percent'high, 8),
+                                to_unsigned(0, 1), to_unsigned(255, 8)), 8);
+    end function;
+
+    function servo_range_degrees_to_ubyte (
+        degrees : servo_range_degrees
+    ) return ubyte is
+    begin
+        return resize(map_range(to_unsigned(degrees, 8), to_unsigned(servo_range_degrees'low, 1), to_unsigned(servo_range_degrees'high, 8),
+                                to_unsigned(0, 1), to_unsigned(255, 8)), ubyte'length);
+    end function;
+
 end utils;
