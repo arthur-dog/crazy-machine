a36fac0fee1fb2229ab8b9e1161ccd18a6abccd6
Ada Stevenson <adanskana@gmail.com>
2025-05-05 19:43:39 +0800

    top_level: added preliminary test code

--- src/vhdl/top_level.vhd
+++ src/vhdl/top_level.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+library work;
+use work.utils.all;
 
 entity top_level is
     Port (
@@ -29,3 +31,26 @@ entity top_level is
         --GPIO[2]: Reset
     );
 end top_level;
+
+architecture rtl of top_level is
+
+    constant duty_repr : ubyte := percent_to_ubyte(30);
+
+    signal clk : std_logic;
+
+begin
+    clock_div_inst : entity work.clock_divider(rtl)
+        port map (
+            clk_50MHz          => MAX10_CLK1_50,
+            reset              => GPIO(2),
+            clk_divider_factor => (others => '0'),
+            clk_out            => clk
+        );
+    pwm_basic : entity work.pwm(base)
+        port map (
+            clk_in => clk,
+            reset => GPIO(2),
+            duty => duty_repr,
+            pwm_out => GPIO(3)
+        );
+end rtl;
