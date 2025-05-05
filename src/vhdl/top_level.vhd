eed1354bbbcad1a44673e26a76a8bf0b82933550
Ada Stevenson <adanskana@gmail.com>
2025-05-05 18:53:07 +0800

    top_level: add comments for pin assignments

--- src/vhdl/top_level.vhd
+++ src/vhdl/top_level.vhd
@@ -6,5 +6,24 @@ entity top_level is
     Port (
         MAX10_CLK1_50 : in    std_logic;
         GPIO          : inout std_logic_vector(35 downto 0)
+        --Section 1:
+        --GPIO[3]: servo
+        --GPIO[1]: FSR
+        --Section 2:
+        --GPIO[5]: Limit sw
+        --GPIO[7]: Servo 1
+        --GPIO[9]: Line sensor
+        --GPIO[11]: Servo 2
+        --Section 3:
+        --GPIO[13]: DC control[0]
+        --GPIO[15]: DC control[1]
+        --GPIO[17]: DC control[2]
+        --GPIO[19]: DC control[3]
+        --Section 4:
+        --GPIO[27]: Line sensor
+        --GPIO[29]: Stepper[0]
+        --GPIO[31]: Stepper[1]
+        --GPIO[33]: Stepper[2]
+        --GPIO[35]: Stepper[3]
     );
 end top_level;
