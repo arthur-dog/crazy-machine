c0c1d6901333a5718be1e075193a43094ee84200
Ada Stevenson <adanskana@gmail.com>
2025-05-09 19:46:34 +0800

    pwm: update to use new mapping funcs, bug fixes

--- src/vhdl/pwm.vhd
+++ src/vhdl/pwm.vhd
@@ -18,24 +18,30 @@ end pwm;
 
 architecture base of pwm is
 
-    signal pwm_count : ubyte;
+    constant PWM_PERIOD : natural := BASE_CLOCK / 50000;
+
+    subtype duty_hertz is natural range 0 to PWM_PERIOD;
+
+    signal pwm_count : duty_hertz;
 
 begin
     pwm_process : process (clk_in)
     begin
         if rising_edge(clk_in) then
             if reset = '1' then
-                pwm_count <= (others => '0');
+                pwm_count <= 0;
                 pwm_out   <= '0';
             else
-                pwm_count <= pwm_count + 1;
-                pwm_out   <= '0';
-                if pwm_count = unsigned(to_signed(-2, pwm_count'length)) then
-                    -- value just before all ones so we dont go over one
-                    pwm_count <= (others => '0');
+                pwm_out <= '0';
+                if pwm_count < PWM_PERIOD then
+                    pwm_count <= pwm_count + 1;
+                else
+                    pwm_count <= 0;
                 end if;
-                if pwm_count < duty then
+                if pwm_count < map_from_zero_natural_range(to_integer(duty), 255, duty_hertz'high) then
                     pwm_out <= '1';
+                else
+                    pwm_out <= '0';
                 end if;
             end if;
         end if;
@@ -58,8 +64,7 @@ architecture servo of pwm is
         duty_byte : ubyte
     ) return duty_hertz is
     begin
-        return to_integer(map_range(duty_byte, to_unsigned(0, 1), to_unsigned(255, ubyte'length),
-                                    to_unsigned(duty_hertz'low, 1), to_unsigned(duty_hertz'high, 24)));
+        return map_from_zero_natural_range(to_integer(duty_byte), UBYTE_SIZE, duty_hertz'high);
     end function;
 
     signal pwm_count   : natural range 0 to PWM_PERIOD - 1;
