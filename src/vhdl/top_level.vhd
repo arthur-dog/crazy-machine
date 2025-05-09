d6f715babe51700a2cbf6e84ac8347a9c6e4fc17
Ada Stevenson <adanskana@gmail.com>
2025-05-09 19:48:20 +0800

    sim, top_level: modify testing

--- src/vhdl/top_level.vhd
+++ src/vhdl/top_level.vhd
@@ -34,24 +34,31 @@ end top_level;
 
 architecture rtl of top_level is
 
-    constant duty_repr     : ubyte    := percent_to_ubyte(30);
-    constant clock_divisor : unsigned := to_unsigned(0, 4);
+    signal duty_repr     : ubyte    := percent_to_ubyte(30);
+    signal divided_clk : std_logic;
 
-    signal clk : std_logic;
+    constant clock_divisor : unsigned := "1000";
+    alias clk_50MHz : std_logic is MAX10_CLK1_50;
 
 begin
     clock_div_inst : entity work.clock_divider(rtl)
         port map (
-            clk_50MHz          => MAX10_CLK1_50,
+            clk_50MHz          => clk_50MHz,
             reset              => GPIO(2),
             clk_divider_factor => std_logic_vector(clock_divisor),
-            clk_out            => GPIO(3)
+            clk_out            => divided_clk);
+    pwm_basic : entity work.pwm(base)
+        port map (
+            clk_in => clk_50MHz,
+            reset => GPIO(2),
+            duty => duty_repr,
+            pwm_out => GPIO(3)
         );
-    -- pwm_basic : entity work.pwm(base)
-    --     port map (
-    --         clk_in => clk,
-    --         reset => GPIO(2),
-    --         duty => duty_repr,
-    --         pwm_out => GPIO(3)
-    --     );
+
+    cycle_duty : process (divided_clk)
+    begin
+        if rising_edge(divided_clk) then
+            duty_repr <= duty_repr + 10;
+            end if;
+    end process;
 end rtl;
