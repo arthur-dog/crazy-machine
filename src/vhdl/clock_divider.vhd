5db81832089f0584984fcfbda4bcfb39cfdada0b
Ada Stevenson <adanskana@gmail.com>
2025-05-12 20:44:03 +0800

    clock_divider: upgrade dividing capability

--- src/vhdl/clock_divider.vhd
+++ src/vhdl/clock_divider.vhd
@@ -6,14 +6,14 @@ entity clock_divider is
     port(
         clk_50MHz          : in  std_logic;
         reset              : in  std_logic;
-        clk_divider_factor : in  std_logic_vector(3 downto 0);
+        clk_divider_factor : in  unsigned(31 downto 0);
         clk_out            : out std_logic);
 end clock_divider;
 
 architecture rtl of clock_divider is
-    signal r_clk_counter      : unsigned(3 downto 0);
-    signal r_clk_divider      : unsigned(3 downto 0);
-    signal r_clk_divider_half : unsigned(3 downto 0);
+    signal r_clk_counter      : unsigned(31 downto 0);
+    signal r_clk_divider      : unsigned(31 downto 0);
+    signal r_clk_divider_half : unsigned(31 downto 0);
 begin
     p_clk_divider : process(clk_50MHz)
     begin
@@ -24,8 +24,8 @@ begin
                 r_clk_divider_half <= (others => '0');
                 clk_out            <= '0';
             else
-                r_clk_divider      <= unsigned(clk_divider_factor)-1;
-                r_clk_divider_half <= unsigned('0'&clk_divider_factor(3 downto 1));  -- half
+                r_clk_divider      <= clk_divider_factor - 1;
+                r_clk_divider_half <= shift_right(clk_divider_factor, 1);  -- half
                 if r_clk_counter < r_clk_divider_half then
                     r_clk_counter <= r_clk_counter + 1;
                     clk_out       <= '0';
