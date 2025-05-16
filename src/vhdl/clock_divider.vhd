a89069d320b33417f9d7958366674a52777c656f
Ada Stevenson <adanskana@gmail.com>
2025-05-16 18:53:23 +0800

    clock_divider: add passthrough functionality

--- src/vhdl/clock_divider.vhd
+++ src/vhdl/clock_divider.vhd
@@ -18,6 +18,9 @@ architecture base of clock_divider is
     signal r_clk_counter      : unsigned(31 downto 0);
     signal r_clk_divider      : unsigned(31 downto 0);
     signal r_clk_divider_half : unsigned(31 downto 0);
+
+    signal result_clk : std_logic;
+
 begin
     p_clk_divider : process(clk_50MHz)
     begin
@@ -26,21 +29,25 @@ begin
                 r_clk_counter      <= (others => '0');
                 r_clk_divider      <= (others => '0');
                 r_clk_divider_half <= (others => '0');
-                clk_out            <= '1';
+                result_clk            <= '1';
             else
                 r_clk_divider      <= clk_divider_factor - 1;
                 r_clk_divider_half <= shift_right(clk_divider_factor, 1);  -- half
                 if r_clk_counter < r_clk_divider_half then
                     r_clk_counter <= r_clk_counter + 1;
-                    clk_out       <= '0';
+                    result_clk       <= '0';
                 elsif r_clk_counter = r_clk_divider then
                     r_clk_counter <= (others => '0');
-                    clk_out       <= '1';
+                    result_clk       <= '1';
                 else
                     r_clk_counter <= r_clk_counter + 1;
-                    clk_out       <= '1';
+                    result_clk       <= '1';
                 end if;
             end if;
         end if;
     end process p_clk_divider;
+
+    -- have passthrough capability
+    clk_out <= result_clk when clk_divider_factor > 0 else clk_50MHz;
+
 end base;
