0883bf6d3c736d2f551d95127fab6354eef1ac8d
Ada Stevenson <adanskana@gmail.com>
2025-05-16 15:32:03 +0800

    pwm: add comments

--- src/vhdl/pwm.vhd
+++ src/vhdl/pwm.vhd
@@ -19,6 +19,8 @@ end pwm;
 
 architecture base of pwm is
 
+    -- we want 50Hz for our PWM, so divide our base clock by 50 to get the
+    -- amount of clock cycles needed
     constant PWM_PERIOD : natural := BASE_CLOCK / 50;
 
     subtype duty_hertz is natural range 0 to PWM_PERIOD;
@@ -34,11 +36,13 @@ begin
             if reset = '1' then
                 pwm_count   <= 0;
                 pwm_out     <= '0';
-                stored_duty <= duty;
+                stored_duty <= duty;  -- we do this so we dont change the duty
+                -- of a wave mid period, which would mess
+                -- with its form.
             else
                 pwm_out <= '0';
                 if pwm_count < PWM_PERIOD then
-                    pwm_count <= pwm_count + 1;
+                    pwm_count <= pwm_count + 1;  -- creates the period
                 else
                     pwm_count   <= 0;
                     stored_duty <= duty;
@@ -52,6 +56,9 @@ begin
         end if;
     end process;
 
+    -- provide a clock matching the period of the PWM signal. taking the PWM
+    -- signal directly as a clock for the duty_manager for some reason does not
+    -- work, although it would be the more elegant solution.
     sync_process : process (clk_in)
     begin
         if rising_edge(clk_in) then
@@ -70,6 +77,8 @@ architecture servo of pwm is
     constant TWO_MS_FREQ    : natural := 1000 / 2;
     constant TWENTY_MS_FREQ : natural := 1000 / 20;
 
+    -- calculate the number of clock cycles needed to get a pulse of 1ms, 2ms,
+    -- and the 20ms period
     constant PWM_PERIOD  : natural := BASE_CLOCK / TWENTY_MS_FREQ;
     constant PWM_0_DEG   : natural := BASE_CLOCK / ONE_MS_FREQ;
     constant PWM_180_DEG : natural := BASE_CLOCK / TWO_MS_FREQ;
@@ -83,7 +92,7 @@ architecture servo of pwm is
         return map_range(to_integer(duty_byte), 0, UBYTE_SIZE, duty_hertz'low, duty_hertz'high);
     end function;
 
-    signal pwm_count   : natural range 0 to PWM_PERIOD - 1;
+    signal pwm_count   : natural range 0 to PWM_PERIOD - 1;  -- so we dont go over
     signal pulse_hertz : duty_hertz;
     signal stored_duty : ubyte := duty;
 
