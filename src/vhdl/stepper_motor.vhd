2f489f847d4136e28f12d77613454a2a463c58d9
Ada Stevenson <adanskana@gmail.com>
2025-05-17 17:17:45 +0800

    stepper_motor: add functionality to specify hertz

--- src/vhdl/stepper_motor.vhd
+++ src/vhdl/stepper_motor.vhd
@@ -6,6 +6,11 @@ library work;
 use work.utils.all;
 
 entity stepper_motor is
+    generic (
+        BASE_CLOCK    : natural := BASE_CLOCK_PHYS;
+        STEPPER_HERTZ : natural := 100
+
+    );
     port (
         clk_in           : in  std_logic;
         reset            : in  std_logic;
@@ -16,12 +21,24 @@ end stepper_motor;
 
 architecture base of stepper_motor is
 
+    constant CLOCK_DIVISOR : natural := BASE_CLOCK / STEPPER_HERTZ;
+
+    signal divided_clk : std_logic;
+
     signal stepper_code : unsigned(3 downto 0) := "0011";
 
 begin
-    stepper_process : process (clk_in)
+
+    clock_divider_inst : entity work.clock_divider(base)
+        port map (
+            clk_50MHz          => clk_in,
+            reset              => reset,
+            clk_divider_factor => to_unsigned(CLOCK_DIVISOR, 32),
+            clk_out            => divided_clk);
+
+    stepper_process : process (divided_clk)
     begin
-        if rising_edge(clk_in) then
+        if rising_edge(divided_clk) then
             if reset = '1' then
                 stepper_code <= "0011";
             else
